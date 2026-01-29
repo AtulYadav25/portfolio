143b85cc1995348e053c4c9d5351fefde71b4db0
Atul <atul007414@gmail.com>
2026-01-29 19:08:20 +0530

    Added OG Image and Meta Title, descriptions for better SEO and Profile sharing through link

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,57 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
 });
 
-
-
 export const metadata: Metadata = {
+  metadataBase: new URL("https://atulyadav.vercel.com"),
   title: "Atul Yadav | Full Stack Developer",
   description:
-    "I build web applications using TypeScript, React, Next.js and MongoDB. Full Stack Developer specializing in UI-focused products and blockchain/web3.",
+    "I build web applications using TypeScript, React, Next.js and MongoDB. Full Stack Developer specializing in UI-focused products and Web3.",
+
   keywords: [
     "Full Stack Developer",
-    "React",
-    "Next.js",
+    "React Developer",
+    "Next.js Developer",
     "TypeScript",
     "MongoDB",
-    "Web3",
+    "Web3 Developer",
   ],
+
   authors: [{ name: "Atul Yadav" }],
+
   openGraph: {
     title: "Atul Yadav | Full Stack Developer",
     description:
-      "I build web applications using TypeScript, React, Next.js and MongoDB.",
+      "Full Stack Developer building modern UI-focused web applications using React, Next.js, TypeScript, and Web3.",
+    url: "https://atulyadav.vercel.com",
+    siteName: "Atul Yadav Portfolio",
+    images: [
+      {
+        url: "./og-image.png",
+        width: 758,
+        height: 758,
+        alt: "Atul Yadav - Full Stack Web Developer",
+      },
+    ],
+    locale: "en_US",
     type: "website",
   },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Atul Yadav | Full Stack Web Developer",
+    description:
+      "I build web applications using TypeScript, React, Next.js, MongoDB, and Web3.",
+    images: ["./og-image.png"],
+    creator: "@atulcode",
+  },
+
   icons: {
     icon: "./favicon2.ico",
   },
@@ -36,20 +59,21 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en" className="dark">
       <head>
         <link
           rel="stylesheet"
-          type="text/css"
           href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
         />
       </head>
-      <Analytics />
-      <body className={`${inter.variable} antialiased`}>{children}</body>
+      <body className={`${inter.variable} antialiased`}>
+        <Analytics />
+        {children}
+      </body>
     </html>
   );
 }
